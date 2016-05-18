7d721b80cf2fb1bd5a10513058a93ce2ed08fe21
huangyuan <wshy1121@qq.com>
2016-05-18 15:23:09 +0800

    [add]添加udp测试代码 by huangyuan

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "ClusterManager.h"
+#include "cli_client.h"
 
 int main()
 {
+    CCliUdpClient::instance()->setSerInf("192.168.85.255", 9000);
+    int sendNum = CCliUdpClient::instance()->send("help\r\n", 7);
+    printf("sendNum  %d\n", sendNum);
     return 0;
 }
 
